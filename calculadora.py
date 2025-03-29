e5752da48b92dd452244ae4783e789e6248843a9
Eduardo de Rivero <eduardomanrique@ravn.co>
2025-03-29 10:30:38 -0500

    feat: testing con python

---
+++ calculadora.py
@@ -0,0 +1,20 @@
+# REALIZAR UNA FUNCION DE SUMATORIA O RESTA en la cual reciba n parametros
+# SUMAR | RESTAR
+# LIST | TUPLE | DICT | SET
+def calculadora(operacion:str, *parametros:tuple[int,...] ) -> int:
+    resultado = 0
+    if operacion == 'SUMA':
+        # resultado = sum(parametros)
+        for numero in parametros:
+            resultado += numero
+
+    elif operacion == 'RESTA':
+        resultado = parametros[0]
+        for numero in parametros[1:]:
+            resultado -= numero
+
+    else:
+        raise 'Operacion incorrecta'
+
+    return resultado
+
